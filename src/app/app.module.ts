14c9293fc6822892133977db9c979d51947e2273
Amit <amitlus7@gmail.com>
2023-02-09 18:21:10 +0200

    Arrange the files in directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DescriptionComponent } from './description.component';
-import { ImageToFourierComponent } from './image-to-fourier.component';
-import { DrawingToFourierComponent } from './drawing-to-fourier.component';
+import { DescriptionComponent } from './description/description.component';
+import { ImageToFourierComponent } from './image-to-fourier/image-to-fourier.component';
+import { DrawingToFourierComponent } from './drawing-to-fourier/drawing-to-fourier.component';
 
 @NgModule({
   declarations: [
